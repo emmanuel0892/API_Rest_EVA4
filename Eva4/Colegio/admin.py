21cbcc84fc79ec75be74a642c88584792f7bd150
Emmanuel Rubio Gaete <emmanuel.rubio.gaete92@gmail.com>
2023-01-05 13:36:59 -0300

    Creacion models y admin

--- Eva4/Colegio/admin.py
+++ Eva4/Colegio/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from Colegio.models import Nivel_Academico,Asignaturas,Docentes,Alumnos,Cursos
 
-# Register your models here.
+class Nivel_Academico_Admin(admin.ModelAdmin):
+    list_display=['cod_nivelA','nombre_nivel']
+
+admin.site.register(Nivel_Academico, Nivel_Academico_Admin)
+
+class Asignaturas_Admin(admin.ModelAdmin):
+    list_display=['cod_asignatura','nombre']
+
+admin.site.register(Asignaturas,Asignaturas_Admin)
+
+class Docentes_Admin(admin.ModelAdmin):
+    list_display=['rutDocente','nombre','apellidoP','apellidoM','sexo']
+
+admin.site.register(Docentes,Docentes_Admin)
+
+class Alumnos_Admin(admin.ModelAdmin):
+    list_display=['rutAlumno','nombre','apellidoP','apellidoM','sexo','nivel_academico']
+
+admin.site.register(Alumnos,Alumnos_Admin)
+
+class Cursos_Admin(admin.ModelAdmin):
+    list_display=['id_curso','nivel_academico','rutDocente','asignatura']
+
+admin.site.register(Cursos,Cursos_Admin)
--- Eva4/Colegio/models.py
+++ Eva4/Colegio/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Nivel_Academico(models.Model):
+    cod_nivelA = models.AutoField(primary_key=True)
+    nombre_nivel = models.TextField(max_length=50)
+
+    def __str__(self):
+        return str(self.nombre_nivel)
+
+class Asignaturas(models.Model):
+    cod_asignatura = models.AutoField(primary_key=True)
+    nombre = models.TextField(max_length=50)
+
+    def __str__(self):
+        return str(self.nombre)
+
+class Docentes(models.Model):
+    rutDocente = models.CharField(max_length=10, primary_key=True)
+    nombre = models.TextField(max_length=50)
+    apellidoP = models.TextField(max_length=50)
+    apellidoM = models.TextField(max_length=50)
+    sexo = models.TextField(max_length=50)
+
+    def __str__(self):
+        return str(self.rutDocente) + "  " + str(self.nombre) + "  " + str(self.apellidoP) + "  " + str(self.apellidoM)
+
+class Alumnos(models.Model):
+    rutAlumno = models.CharField(max_length=10, primary_key=True)
+    nombre = models.TextField(max_length=50)
+    apellidoP = models.TextField(max_length=50)
+    apellidoM = models.TextField(max_length=50)
+    sexo = models.TextField(max_length=50)
+    nivel_academico = models.ForeignKey(Nivel_Academico, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.rutAlumno) + " - " + str(self.nombre) + " - " + str(self.apellidoP) + " - " + str(self.apellidoM) + " - " + str(self.sexo) + " - " + str(self.nivel_academico)
+
+class Cursos(models.Model):
+    id_curso = models.AutoField(primary_key=True)
+    nivel_academico = models.ForeignKey(Nivel_Academico, on_delete=models.CASCADE)
+    rutDocente = models.ForeignKey(Docentes, on_delete=models.CASCADE)
+    asignatura = models.ForeignKey(Asignaturas, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id_curso) + " - " + str(self.nivel_academico) + " - " + str(self.rutDocente) + " - " + str(self.asignatura)
